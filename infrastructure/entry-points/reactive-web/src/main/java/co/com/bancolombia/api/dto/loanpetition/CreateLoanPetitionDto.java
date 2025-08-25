1e95ceced254ca5b98e17509f5d6627f7cbbf11f
Walter Moises Moises Molina <w7moises@gmail.com>
2025-08-24 19:56:58 -0500

    add swagger operation for all the controllers

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/loanpetition/CreateLoanPetitionDto.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/loanpetition/CreateLoanPetitionDto.java
@@ -1,10 +1,20 @@
 package co.com.bancolombia.api.dto.loanpetition;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 public record CreateLoanPetitionDto(
+        @NotNull(message = "{amount.required}")
+        @DecimalMax(value = "999999999", inclusive = false, message = "{amount.max}")
+        @DecimalMin(value = "0", inclusive = false, message = "{amount.max}")
         BigDecimal amount,
+        @NotNull(message = "{term.required}")
         Integer term,
+        @NotBlank(message = "{document.required}")
         String documentNumber
 ) {
 }
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/loanpetition/EditLoanPetitionDto.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/loanpetition/EditLoanPetitionDto.java
@@ -1,10 +1,16 @@
 package co.com.bancolombia.api.dto.loanpetition;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 public record EditLoanPetitionDto(
+        @NotNull(message = "{amount.required}")
         BigDecimal amount,
+        @NotNull(message = "{term.required}")
         Integer term,
+        @NotBlank(message = "{document.required}")
         String documentNumber
 ) {
 }
