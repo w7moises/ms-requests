e42b4dd8d67317c2e40300c8311428cd2065e124
Walter Moises Moises Molina <w7moises@gmail.com>
2025-08-24 21:44:26 -0500

    java version and beans added for testing evaluation

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/mapper/LoanTypeDtoMapper.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/mapper/LoanTypeDtoMapper.java
@@ -5,10 +5,11 @@ import co.com.bancolombia.api.dto.loantype.EditLoanTypeDto;
 import co.com.bancolombia.api.dto.loantype.LoanTypeDto;
 import co.com.bancolombia.model.loantype.LoanType;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface LoanTypeDtoMapper {
     LoanTypeDto toResponse(LoanType loanType);
 
